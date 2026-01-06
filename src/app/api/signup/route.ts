8928e43ad2f3568762e4fc1db4a57ceb82566a2c
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-06 16:02:48 +0600

    check your name is already taken

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -7,11 +7,29 @@ export async function POST(request: Request) {
   await dbConnect();
 
   try {
+    const { username, email, password } = await request.json();
+
+    const existingUserVerifiedByUserName = await userModel.findOne({
+      username,
+      isVerified: true,
+    });
+
+    if (existingUserVerifiedByUserName) {
+      return Response.json(
+        { success: false, message: "User name is already taken" },
+        { status: 400 }
+      );
+    }
   } catch (error) {
     console.error("Error Registering user", error);
-    return Response.json({
-      success: false,
-      message: "Error registering user",
-    });
+    return Response.json(
+      {
+        success: false,
+        message: "Error registering user",
+      },
+      {
+        status: 500,
+      }
+    );
   }
 }
